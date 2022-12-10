2c922d52d7dc15ce1eec977dc950b15fae9c0139
kwil3 <80280841+kwil3@users.noreply.github.com>
2022-12-09 20:22:50 -0600

    added server folder and added controllers and bin

---
+++ BlazorApp2/Server/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using BlazorApp2.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ApplicationDbContext Context;
+        private readonly ILogger<ValuesController> _logger;
+        public ValuesController(ApplicationDbContext context, ILogger<ValuesController> logger)
+        {
+            Context = context;
+            _logger = logger;
+        }
+        [HttpGet("CheckStatus/{url}")]
+        public async Task<bool> CheckStatus(string url)
+        {
+            var ping = new System.Net.NetworkInformation.Ping();
+
+            var result = ping.Send(url);
+
+            if (result.Status != System.Net.NetworkInformation.IPStatus.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+              
+        }
+
+        [HttpPost("AddLogs")]
+        public async Task<string> AddLogs(tbllog log)
+        {
+            if (log.Name != string.Empty || log.Checked != string.Empty || log.Website != string.Empty)
+            {
+
+                var id = Context.Add(log);
+                await Context.SaveChangesAsync();
+                return "Added Success";
+            }
+            else
+            {
+                return "Error while adding";
+            }
+
+
+
+
+        }
+        [HttpGet("GetList")]
+        public async Task<List<tbllog>> GetLogs()
+        {
+            List<tbllog> Logs = new List<tbllog>();
+
+            Logs = Context.tblLog.ToList();
+
+            return Logs;
+
+
+        }
+    }
+}
